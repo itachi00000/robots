9e260a11029e87946a2f4a5a5fbce122551dace8
itachi00000 <>
2019-12-12 15:16:06 +0800

    Updates

---
+++ precache-manifest.e7eb572fd0045576564a55a94b358a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4bbb2414a793225b9f678c134c19fb1",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "83d50b1939762324e8fe",
+    "url": "/robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f0ac25abbe8ea687e24d",
+    "url": "/robots/static/css/main.e5c39c91.chunk.css"
+  },
+  {
+    "revision": "83d50b1939762324e8fe",
+    "url": "/robots/static/js/2.b98f27c7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robots/static/js/2.b98f27c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0ac25abbe8ea687e24d",
+    "url": "/robots/static/js/main.79a0beae.chunk.js"
+  },
+  {
+    "revision": "706097e854eeb1b00248",
+    "url": "/robots/static/js/runtime-main.9155c969.js"
+  }
+]);
